abac4229181dcd128b4a52cdedae008b3b8d8e33
rifkyalwansyah <147989803+rifkyalwansyah@users.noreply.github.com>
2024-12-09 02:25:42 +0700

    Create database.php

---
+++ database.php
@@ -0,0 +1,34 @@
+<?php
+class Database
+{
+    protected $host;
+    protected $user;
+    protected $password;
+    protected $db_name;
+    protected $conn;
+
+    public function __construct()
+    {
+        $this->getConfig();
+        $this->conn = new mysqli($this->host, $this->user, $this->password, $this->db_name);
+
+        if ($this->conn->connect_error) {
+            die("Koneksi Gagal: " . $this->conn->connect_error);
+        }
+    }
+
+    private function getConfig()
+    {
+        include_once("config.php");
+        $this->host = $config['host'];
+        $this->user = $config['username'];
+        $this->password = $config['password'];
+        $this->db_name = $config['db_name'];
+    }
+
+    public function query($sql)
+    {
+        return $this->conn->query($sql);
+    }
+}
+?>
